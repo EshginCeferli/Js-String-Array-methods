f91ddd050bfffb61c940eb14ac42eadd7143e5d7
EshginCeferli <eshginij@code.edu.az>
2022-08-23 23:57:28 +0400

    Array,String-methods

---
+++ assets/js/script.js
@@ -0,0 +1,107 @@
+// let cities = "Baki, Istanbul, Ankara";
+// console.log(cities.slice(6,14));
+// console.log(cities.slice(-6,-2));
+// console.log(cities.slice(6));
+// console.log(cities.substring(-2));
+// console.log(cities.substr(6,8));
+
+
+// let text = "Go to Home Home"
+// console.log(text);
+// console.log(text.replace("Home", "School"));
+// console.log(text.replace("Hme","School"));
+// console.log(text.replace("g/ Home", "School"));
+
+// let test = "test"
+// let test1 = "TEST"
+
+// console.log(test.toLocaleUpperCase());
+// console.log(test1.toLocaleLowerCase());
+
+// let join = test.concat("", test1);
+// console.log(join);
+
+// let cities = "  Baki, Istanbul, Ankara  ";
+// console.log(cities.trim());
+// console.log(cities.trimEnd());
+
+// let letter = "a";
+// let num = "1";
+// console.log(letter.padStart(4,num));
+// console.log(letter.padEnd(4,num));
+
+// let char = "Z";
+// console.log(char.charCodeAt(0));
+
+// let cities = "Baki, Baki, Istanbul, Ankara";
+// console.log(cities.split(","));
+
+// console.log(cities.split());
+// console.log(cities.split(""));
+
+// console.log(cities.indexOf("Istanbul Ankara"));
+// console.log(cities.lastIndexOf("Baki"));
+// console.log(cities.indexOf("Istanbul",20));
+
+// let search = cities.match("a")
+// console.log(search);
+// console.log(cities.includes("i",15));
+
+// let number = 10;
+// console.log(number);
+
+// console.log(number.toString());
+
+// let decimal = 10.5555;
+// console.log(decimal.toExponential(2));
+// console.log(decimal.toExponential(0));
+
+// console.log(decimal.toFixed(2));
+
+// console.log(decimal.toPrecision(4));
+
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number(" "));
+// console.log(Number("Veli"));
+// console.log(Number("10 20"));
+
+// let date = new Date("2001-26-06")
+// console.log(date);
+
+
+// console.log(parseFloat("104 44"));
+// console.log(parseFloat("Melik"));
+// console.log();
+
+// let cities = ["Baki", "Istanbul", "Ankara"];
+// let countries = ["Turkey", "Russia", "Italy"];
+
+// console.log(cities.toString());
+// console.log(cities.join("-"));
+
+// console.log(cities.pop());
+// console.log(cities);
+
+// console.log(cities.push("Gence"));
+// console.log(cities);
+
+// let all = cities.concat(countries);
+// let all = cities.concat(countries,"Qazax");
+// console.log(all);
+
+// let cities = ["Baki", "Istanbul", "Ankara", "Qazax","Kurdemir"];
+// cities.splice(2,1,"Quba");
+// console.log(cities);
+// cities.splice(0,1);
+// console.log(cities);
+
+// console.log(cities.slice(1,3));
+
+// console.log(cities.reverse());
+// console.log(cities.sort());
+// console.log(cities.reverse().sort());
+
+// let points = [40, 100, 1, 5, 25, 10];
+// console.log(points.sort(function(b, a){return a - b})
+// );
